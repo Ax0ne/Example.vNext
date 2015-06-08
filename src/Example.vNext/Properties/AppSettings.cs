9303bfaa320ce9ca53b939f8cacba1eab82478c6
Ax0ne <Ax0ne@live.com>
2015-06-08 11:02:45 +0800

    新增设置对象

---
+++ src/Example.vNext/Properties/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.vNext.Properties
+{
+    public class AppSettings
+    {
+        public string SiteTitle { get; set; }
+    }
+}
